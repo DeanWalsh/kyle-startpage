250c9f361f4088aadd88b533e8acda71cc2c24b2
DeanWalsh <deanochambers@gmail.com>
2022-08-29 04:09:33 +0100

    init commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const showTime = document.getElementById('time');
+const greeting = document.getElementById('greeting');
+const date = document.getElementById('date')
+time = () => {
+    let d = new Date();
+    let day = d.toDateString();
+    let s = d.getSeconds();
+    let m = d.getMinutes();
+    let h = d.getHours();
+    if (s < 10) {
+        s = "0" + s;
+    }
+    if (m < 10) {
+        m = "0" + m;
+    }
+    if (h < 10) {
+        h = "0" + h;
+    }
+
+    if (h === 0 || h < 7) {
+        greeting.innerText = "Good Night Kyle"
+    } else if (h >= 7 && h < 13) {
+        greeting.innerText = "Good Morning Kyle"
+    } else if (h >= 13 && h < 19) {
+        greeting.innerText = "Good Afternoon Kyle"
+    } else {
+        greeting.innerText = "Good Evening Kyle"
+    }
+
+    date.innerHTML = day
+    showTime.textContent = h + ":" + m + ":" + s;
+}
+time();
+setInterval(time, 1000);
